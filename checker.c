97dcf8b2a1e695740cdc08e6512ab0e7facf2924
SnehA-ShettY <95692157+SnehA-ShettY@users.noreply.github.com>
2024-08-23 10:33:31 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 #include <assert.h>
 
-int range_check(float value, float min, float max, const char* message) {
-    if (value < min || value > max) {
+int range_check(float value, float min, float max, const char* message) 
+{    
+    if (value < min || value > max) 
+    {
         printf("%s\n", message);
         return 1;
     }
+    
     return 0;
 }
 
 int batteryIsOk(float temperature, float soc, float charge_rate) {
+{
     int temp_range_check, soc_range_check, charge_rate_range_check;
+    
     temp_range_check = range_check(temperature, 0, 45, "Temperature out of range!");
     soc_range_check = range_check(soc, 20, 80, "State of Charge out of range!");
     charge_rate_range_check = range_check(chargeRate, 0, 0.8, "Charge Rate out of range!");
@@ -19,6 +24,7 @@ int batteryIsOk(float temperature, float soc, float charge_rate) {
 }
 
 int main() {
+{
     assert(batteryIsOk(25, 70, 0.7));
     assert(!batteryIsOk(50, 85, 0));
 }
