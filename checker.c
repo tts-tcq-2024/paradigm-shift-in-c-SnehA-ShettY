7c7810f2ed0b87b030ccf4d9915c985094403510
SnehA-ShettY <95692157+SnehA-ShettY@users.noreply.github.com>
2024-08-23 10:24:55 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -13,9 +13,9 @@ int batteryIsOk(float temperature, float soc, float chargeRate) {
     int temp_range_check;
     int soc_range_check;
     int charge_rate_check;
-    int temp_range_check = range_check(temperature, 0, 45, "Temperature out of range!");
-    int soc_range_check = range_check(soc, 20, 80, "State of Charge out of range!");
-    int charge_rate_check = range_check(chargeRate, 0, 0.8, "Charge Rate out of range!");
+    temp_range_check = range_check(temperature, 0, 45, "Temperature out of range!");
+    soc_range_check = range_check(soc, 20, 80, "State of Charge out of range!");
+    charge_rate_check = range_check(chargeRate, 0, 0.8, "Charge Rate out of range!");
 
     return !(temp_range_check || soc_range_check || charge_rate_check);
 }
